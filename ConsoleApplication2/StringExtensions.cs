1a144b7c386109deb9aa79a5d51ee4c728b92013
Marzepalla <tklimoshenko@mail.ru>
2016-03-23 13:10:16 +0200

    Task 1

---
+++ ConsoleApplication2/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace Test_task_1
+{
+    public static class StringExtensions
+    {
+        public static int GetLenghtOfMaxSubsequence(this string stringToSearch, char charToFind)
+        {
+            int counter = 0;
+            int max = 0;
+            int i = 0;
+
+            while (i < stringToSearch.Length)
+            {
+                if (stringToSearch[i] == charToFind)
+                {
+                    counter = 0;
+
+                    while (i < stringToSearch.Length && stringToSearch[i] == charToFind)
+                    {
+                        counter++;
+                        i++;
+                    }
+
+                    if (max < counter)
+                    {
+                        max = counter;
+                    }
+                }
+
+                i++;
+            }
+
+            return max;
+        }
+    }
+}
