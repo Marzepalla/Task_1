74ebec6b3d67daafa1cb7febc66e0edd42ac92d8
Marzepalla <tklimoshenko@mail.ru>
2016-03-23 13:09:17 +0200

    Signed-off-by: Marzepalla <tklimoshenko@mail.ru>

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Test_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Последовательность символов: \n{0}\n", Properties.Resources.Input);
+            Console.WriteLine("Введите символ для поиска самой длинной непрерывной цепочки");
+            char SearchChar = (char)Console.ReadLine()[0];
+
+            int MaxSequence = Properties.Resources.Input.GetLenghtOfMaxSubsequence(SearchChar);
+
+            if (MaxSequence == 0)
+            {
+                string output = "Непрерывной последовательности из символов '" + SearchChar + "' нет!";
+                System.IO.File.WriteAllText("../../Resouces/Output.txt", output);
+                Console.WriteLine(output);
+            }
+            else
+            {
+                string output = "Длина непрерывной последовательности из символов '" + SearchChar + "' : "+ MaxSequence + " знаков \n"+ new string(SearchChar, MaxSequence);
+               System.IO.File.WriteAllText("../../Resouces/Output.txt", output);
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
